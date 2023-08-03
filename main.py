dc479d22bd0ac82394b1e2491caae3b48e3a7128
alstjr7375 <alstjr7375@daum.net>
2023-08-03 20:20:30 +0900

    Add: EasyOCR

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 import cv2
+from abc import ABC, abstractmethod
 from pororo import Pororo
 from pororo.pororo import SUPPORTED_TASKS
 from utils.image_util import plt_imshow, put_text
+from easyocr import Reader
 import warnings
 
 warnings.filterwarnings('ignore')
 
-
-class PororoOcr:
-    def __init__(self, model: str = "brainocr", lang: str = "ko", **kwargs):
-        self.model = model
-        self.lang = lang
-        self._ocr = Pororo(task="ocr", lang=lang, model=model, **kwargs)
+class BaseOcr(ABC):
+    def __init__(self):
         self.img_path = None
         self.ocr_result = {}
+
+    def get_ocr_result(self):
+        return self.ocr_result
+
+    def get_img_path(self):
+        return self.img_path
+
+    def show_img(self):
+        plt_imshow(img=self.img_path)
+
+    def show_img_with_ocr(self, bounding, description, vertices, point):
+        img = cv2.imread(self.img_path)
+        roi_img = img.copy()
+        color = (0, 255, 0)
+
+        x, y = point
+        ocr_result =  self.ocr_result if bounding is None \
+            else self.ocr_result[bounding]
+        for text_result in ocr_result:
+            text = text_result[description]
+            rect = text_result[vertices]
+
+            topLeft, topRight, bottomRight, bottomLeft = [
+                (round(point[x]), round(point[y])) for point in rect
+            ]
+
+            cv2.line(roi_img, topLeft, topRight, color, 2)
+            cv2.line(roi_img, topRight, bottomRight, color, 2)
+            cv2.line(roi_img, bottomRight, bottomLeft, color, 2)
+            cv2.line(roi_img, bottomLeft, topLeft, color, 2)
+            roi_img = put_text(roi_img, text, topLeft[0], topLeft[1] - 20, font_size=15)
+
+        plt_imshow(["Original", "ROI"], [img, roi_img], figsize=(16, 10))
+
+    @abstractmethod
+    def run_ocr(self, img_path: str, debug: bool = False):
+        pass
+
+class PororoOcr(BaseOcr):
+    def __init__(self, model: str = "brainocr", lang: str = "ko", **kwargs):
+        super().__init__()
+        self._ocr = Pororo(task="ocr", lang=lang, model=model, **kwargs)
 
     def run_ocr(self, img_path: str, debug: bool = False):
         self.img_path = img_path
@@ -25,7 +65,7 @@ class PororoOcr:
             ocr_text = "No text detected."
 
         if debug:
-            self.show_img_with_ocr()
+            self.show_img_with_ocr("bounding_poly", "description", "vertices", ["x", "y"])
 
         return ocr_text
 
@@ -37,50 +77,31 @@ class PororoOcr:
     def get_available_models():
         return SUPPORTED_TASKS["ocr"].get_available_models()
 
-    def get_ocr_result(self):
-        return self.ocr_result
+# https://www.jaided.ai/easyocr/documentation/
+class EasyOcr(BaseOcr):
+    def __init__(self, lang: list[str] = ["ko", "en"], gpu=False, **kwargs):
+        super().__init__()
+        self._ocr = Reader(lang_list=lang, gpu=gpu, **kwargs).readtext
 
-    def get_img_path(self):
-        return self.img_path
+    def run_ocr(self, img_path: str, debug: bool = False):
+        self.img_path = img_path
+        self.ocr_result = self._ocr(img_path, detail=1)
 
-    def show_img(self):
-        plt_imshow(img=self.img_path)
+        if len(self.ocr_result) != 0:
+            ocr_text = list(map(lambda result: result[1], self.ocr_result))
+        else:
+            ocr_text = "No text detected."
 
-    def show_img_with_ocr(self):
-        img = cv2.imread(self.img_path)
-        roi_img = img.copy()
+        if debug:
+            self.show_img_with_ocr(None, 1, 0, [0, 1])
 
-        for text_result in self.ocr_result['bounding_poly']:
-            text = text_result['description']
-            tlX = text_result['vertices'][0]['x']
-            tlY = text_result['vertices'][0]['y']
-            trX = text_result['vertices'][1]['x']
-            trY = text_result['vertices'][1]['y']
-            brX = text_result['vertices'][2]['x']
-            brY = text_result['vertices'][2]['y']
-            blX = text_result['vertices'][3]['x']
-            blY = text_result['vertices'][3]['y']
-
-            pts = ((tlX, tlY), (trX, trY), (brX, brY), (blX, blY))
-
-            topLeft = pts[0]
-            topRight = pts[1]
-            bottomRight = pts[2]
-            bottomLeft = pts[3]
-
-            cv2.line(roi_img, topLeft, topRight, (0, 255, 0), 2)
-            cv2.line(roi_img, topRight, bottomRight, (0, 255, 0), 2)
-            cv2.line(roi_img, bottomRight, bottomLeft, (0, 255, 0), 2)
-            cv2.line(roi_img, bottomLeft, topLeft, (0, 255, 0), 2)
-            roi_img = put_text(roi_img, text, topLeft[0], topLeft[1] - 20, font_size=15)
-
-            # print(text)
-
-        plt_imshow(["Original", "ROI"], [img, roi_img], figsize=(16, 10))
-
+        return ocr_text
 
 if __name__ == "__main__":
-    ocr = PororoOcr()
+    p_ocr = PororoOcr()
+    e_ocr = EasyOcr()
     image_path = input("Enter image path: ")
-    text = ocr.run_ocr(image_path, debug=True)
+    text = p_ocr.run_ocr(image_path, debug=True)
+    print('Result :', text)
+    text = e_ocr.run_ocr(image_path, debug=True)
     print('Result :', text)
